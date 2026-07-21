c107dbaa7c47478575ff30f91ee32b3279729883
Willians96 <michel.wmoraes@gmail.com>
2026-07-21 20:44:41 -0300

    fix: cria convex/convex.config.ts (necessario p/ httpActions serem reconhecidas)

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -9,7 +9,7 @@
  */
 
 import type * as auth from "../auth.js";
-import type * as httpActions from "../httpActions.js";
+import type * as http from "../http.js";
 import type * as mutations from "../mutations.js";
 
 import type {
@@ -20,7 +20,7 @@ import type {
 
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
-  httpActions: typeof httpActions;
+  http: typeof http;
   mutations: typeof mutations;
 }>;
 
